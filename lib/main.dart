936d9f118db74e31033dee9ab4e0a02f7d46382a
Om Sharma <justsharmaji69@gmail.com>
2025-07-25 00:39:17 +0530

    Minor Fix

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'screens/login_screen.dart';
 import 'screens/home_screen.dart';
-import 'services/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
